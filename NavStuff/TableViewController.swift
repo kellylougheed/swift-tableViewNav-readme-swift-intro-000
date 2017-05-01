27ec33f85a43313850eb92fea00e013426a62501
Kelly Lougheed <kellylougheed@gmail.com>
2017-05-01 15:06:36 -0700

    Finish Dirty Dancing app

--- NavStuff/TableViewController.swift
+++ NavStuff/TableViewController.swift
@@ -16,10 +16,28 @@ class TableViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return dirtyDancingSoundtrack.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
+        let cell = tableView.dequeueReusableCell(withIdentifier: "DirtyCell", for: indexPath)
+        let songTitle = dirtyDancingSoundtrack[indexPath.row]
+        cell.textLabel?.text = songTitle
+        return cell
     }
+    
+    let dirtyDancingSoundtrack: [String] = [
+        "(I've Had) The Time of My Life",
+        "Be My Baby",
+        "She's Like the Wind",
+        "Hungry Eyes",
+        "STAY",
+        "Yes",
+        "You Don't Own Me",
+        "Hey Baby",
+        "Overload",
+        "Love Is Strange",
+        "Where Are You Tonight?",
+        "In the Still of the Night"
+    ]
 }
